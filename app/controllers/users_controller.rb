0694cc704dc4cde68e4cb3084db70fc3b50415b5
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-11-20 15:12:34 +0000

    [Finish]bookindex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
   end
-    
+
   def show
+    @user = User.find(params[:is])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:is])
   end
 end
