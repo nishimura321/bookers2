27b6efdd53e988a8d2a7889774032077612cfe6d
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-11-21 15:52:51 +0000

    [Finish]usersコントローラ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
 class UsersController < ApplicationController
+
   def index
   end
 
   def show
-    @user = User.find(params[:is])
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
-    @user = User.find(params[:is])
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+  
 end
