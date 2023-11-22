41e518fbb332b0054db38da053315e7bb50628b0
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-11-22 13:22:56 +0000

    [Add]users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def show
@@ -19,9 +20,9 @@ class UsersController < ApplicationController
   end
 
 private
-  
+
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
 end
